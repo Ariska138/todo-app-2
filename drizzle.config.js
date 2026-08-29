058b7f8c0f7739a123bd13532975324b13337d83
M Raihan H <ar937464@gmail.com>
2026-08-29 12:03:57 +0700

    membuat sql dari schema

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 export default {
     dialect: 'postgresql',
-    screma: './db/schema.js',
+    schema: './db/schema.js',
     out: './drizzle',
     dbCredentials: {
         url: process.env.DATABASE_URL,
